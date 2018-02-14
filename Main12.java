d6d9bcde9c0b9d57fb9d41dda186d07e3dee188b
Lime Icing <emil577b@stud.kea.dk>
2018-02-14 13:30:53 +0100

    Assignment prepped

---
+++ Main12.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Main12 {
+
+    /*
+    Write a method contains3 that accepts a list of strings as a parameter and returns true if any single string occurs
+    at least 3 times in the list, and false otherwise. Use a map as auxiliary storage.
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean contains3(LinkedList<String> list) {
+        HashSet<String> aux = new HashSet<>(list);
+        
+    }
+}
