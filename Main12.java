8b63bc39c9566a98535a24c249659e843ece291d
Lime Icing <emil577b@stud.kea.dk>
2018-02-15 22:23:42 +0100

    Assignment done

--- Main12.java
+++ Main12.java
@@ -9,11 +9,35 @@ public class Main12 {
      */
 
     public static void main(String[] args) {
-
+        LinkedList<String> a = new LinkedList<>();
+        a.add("Nakmor");
+        a.add("Ryder");
+        a.add("Harper");
+        a.add("Nyx");
+        a.add("Ryder");
+        a.add("Kosta");
+        System.out.println(contains3(a));
+        a.add("Ryder");
+        System.out.println(contains3(a));
+        a.add("Ryder");
+        System.out.println(contains3(a));
     }
 
     public static boolean contains3(LinkedList<String> list) {
         HashSet<String> aux = new HashSet<>(list);
+
+        for (String setStr:aux) {
+            int counter = 0;
+            for (String listStr:list) {
+                if (setStr.equals(listStr)) {
+                    counter++;
+                    if (counter == 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+
         return false;
     }
 }
